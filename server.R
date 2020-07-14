db1a55686514141feb92f24a7238e757f1b9381c
jh668 <2170cpeast@gmail.com>
2020-07-13 21:15:21 -0700

    edit server.R

--- server.R
+++ server.R
@@ -6,8 +6,11 @@ library(tm)
 source("next_word_model.R")
 
 shinyServer(function(input, output) {
-    output$out <- renderText({
-        next_word <- next_word(input$box1)
+    output$out <- reactive({
+            validate(
+                    need(input$box1, "Please type in your words in the above textbox")
+            )
+            next_word <- next_word(input$box1)
     })
     
 })
